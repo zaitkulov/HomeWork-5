a8066c0d96976d8ced83192947eb818f1f0573bf
zaitkulov <zaitkulov@gmail.com>
2023-12-12 16:00:12 +0500

    HomeWork-5-1.Времена года и их температура

--- HomeWork-5/main.swift
+++ HomeWork-5/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+//1.Задание: Времена года и их температура
+
+enum Season {
+    case winter
+    case spring
+    case summer
+    case fall
+    var averageTemperature: Double {
+        switch self {
+        case.winter:
+            return -18.1
+        case.spring:
+            return 1.5
+        case.summer:
+            return 19.8
+        case.fall:
+            return 11.2
+        }
+    }
+}
+let result1: Season = .summer
+print(result1.averageTemperature)
 
